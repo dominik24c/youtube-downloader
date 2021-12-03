b8e34fa827f06735bad6d193fd3937a233b7413e
dominik24c <dominik24c@gmail.com>
2021-12-03 23:57:04 +0100

    update BasePage class- update init_grid() method - replace zip() func by zip_longest()

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 import abc
 from tkinter import Frame
 
+from itertools import zip_longest
 
 class BasePage(Frame):
     def __init__(self, master: Frame, root, **kwargs):
@@ -13,6 +14,8 @@ class BasePage(Frame):
         raise NotImplemented
 
     def init_gird(self, row_weights: tuple, column_weights: tuple) -> None:
-        for index, (c, r) in enumerate(zip(column_weights, row_weights)):
-            self.columnconfigure(index, weight=c)
-            self.rowconfigure(index, weight=r)
+        for index, (c, r) in enumerate(zip_longest(column_weights, row_weights)):
+            if c is not None:
+                self.columnconfigure(index, weight=c)
+            if r is not None:
+                self.rowconfigure(index, weight=r)
